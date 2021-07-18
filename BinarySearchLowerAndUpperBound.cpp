8af354b08095ea9066be5ef9f4454fb0c2ba4e61
Bodh Almast <bodhalmast@gmail.com>
2021-07-18 23:28:34 +0530

    Using binary search. Defined search function and then looked for first or last occurence of 1

--- BinarySearchLowerAndUpperBound.cpp
+++ BinarySearchLowerAndUpperBound.cpp
@@ -1,41 +1,78 @@
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> searchRange(vector<int>& A, int target) {
-        
+
         int n = (int)(A.size());
         
-        int lo = 0; int hi = n-1;
-        // finding lower bound
-        
-        while(lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if(A[mid] >= target) 
-                hi = mid;
-            else 
-                lo = mid+1;
+        if(n == 0) {
+            vector<int> t; t.push_back(-1); t.push_back(-1);
+            return t;
         }
+
+        // first occurence of a number in the sorted array
+        // binary function: A[x] < target
+        // applying above function on array would make it like : [0 0 0 0 1 1 1 1 1]
+        // our job is to find the first 1 in this sequence
         vector<int> ans;
-        // Assert lo == hi
-        
-        if(A[lo] == target) ans.push_back(lo);
-        else {
-            ans.push_back(-1);
-            ans.push_back(-1);
-            return ans;
-        } 
-        
-        lo = 0; hi = n-1;
-        while(lo < hi) {
+
+        int lo = 0; int hi = n - 1;
+        while(hi - lo > 1) {
+            int mid = lo + (hi - lo) / 2;
+            if(A[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        int firstPos = -1;
+        if(A[lo] == target) firstPos = lo;
+        else if(A[hi] == target) firstPos = hi;
+
+        ans.push_back(firstPos);
+
+        // last occurence of a number in a sorted array
+        // binary function : A[x] <= target
+        // applying above function on array would make it like : [1 1 1 1 1 0 0 0 0]
+        // find last 1
+        lo = 0; hi = n - 1;
+        while(hi - lo > 1) {
             int mid = lo + (hi - lo) / 2;
             if(A[mid] <= target) {
                 lo = mid;
             } else {
-                hi = mid-1;
+                hi = mid - 1;
             }
         }
-        
-        if(A[lo] == target) ans.push_back(lo);
+        int lastPos = -1;
+        if(A[hi] == target) lastPos = hi;
+        else if(A[lo] == target) lastPos = lo;
+        ans.push_back(lastPos);
         return ans;
     }
 };
+
+int main() {
+
+    Solution solve;
+    int n; cin >> n;
+    vector<int> A;
+    for(int i = 0; i < n; i++) {
+        int a; cin >> a;
+        A.push_back(a);
+    }
+
+    int target; cin >> target;
+
+    vector<int> ans = solve.searchRange(A, target);
+
+    cout << ans[0] << " : " << ans[1] << endl;
+
+    // solve.searchRange()
+
+}
